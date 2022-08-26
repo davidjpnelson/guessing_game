0ca85ec13b3e97a8760546c66950d55b8dfb344a
djpn <36060426+davidjpnelson@users.noreply.github.com>
2022-08-26 13:52:25 -0500

    fixed print! -> println! loop, matching, error handling full game

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
-
     loop {
         println!("Please input your guess.");
 
@@ -18,16 +16,16 @@ fn main() {
             .read_line(&mut guess)
             .expect("failed to read line");
 
-        let guess: u32 = guess
-            .trim()
-            .parse()
-            .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
-            Ordering::Greater => print!("Too big!"),
+            Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
                 break;
